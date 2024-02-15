06e24056599e1dbe53ef2c4044613b9f71a1d462
Evan Taylor <66334307+taylorec@users.noreply.github.com>
2024-02-15 09:45:59 -0600

    Update abstract_factory.py

--- Creational Design Patterns/abstract_factory.py
+++ Creational Design Patterns/abstract_factory.py
@@ -1,3 +1,9 @@
+'''
+The abstract factory design pattern creates an interface that is used to create multiple
+objects without knowledge of the concrete class. This is a high-powered demonstration
+of encapsulation. This design pattern allows us to change implementations without changing source code.
+'''
+
 
 # Frog game
 
